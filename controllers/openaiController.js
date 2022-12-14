26cd30918de0e58c800a8139721ea7080f7ea28c
farhan ibne saif <farhanibne860@gmail.com>
2022-12-14 12:59:51 +0600

    Update openaiController.js

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -9,7 +9,7 @@ const generateImage = async (req, res) => {
   const { prompt, size } = req.body;
 
   const imageSize =
-    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+    size === 'small' ? '256x256' : '256x256';
 
   try {
     const response = await openai.createImage({
